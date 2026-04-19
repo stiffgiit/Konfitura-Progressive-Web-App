19aea01fbbb0ce8656cf5c83bbe862a9b7371710
geralt245 <konradpep21@gmail.com>
2026-04-19 21:22:42 +0200

    Aktualizacja Service Workera

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'konfitura-cache-v10';
+const CACHE_NAME = 'konfitura-cache-v11';
 
 const ASSETS = [
     './index.html',
+    './app.html',
     './manifest.json',
     './icons/icon-192x192.png',
     './icons/icon-512x512.png',
