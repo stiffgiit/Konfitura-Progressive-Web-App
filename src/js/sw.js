22b02ace112f1bcdcae639d56fce0d790ab161ef
sfron <stasiu.fron2003@gmail.com>
2026-04-19 15:08:17 +0200

    Fix

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = 'konfitura-cache-v5';
+const CACHE_NAME = 'konfitura-cache-v6';
 
 const ASSETS = [
-    '/index.html',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    '/src/css/bootstrap.min.css',
-    '/src/css/leaflet.css',
-    '/src/css/style.css',
-    '/src/js/leaflet.js',
-    '/src/js/bootstrap.bundle.min.js',
-    '/src/js/app.js',
-    '/images/layers.png',
-    '/images/layers-2x.png',
-    '/images/marker-icon.png',
-    '/images/marker-icon-2x.png',
-    '/images/marker-shadow.png',
+    './index.html',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    './src/css/bootstrap.min.css',
+    './src/css/leaflet.css',
+    './src/css/style.css',
+    './src/js/leaflet.js',
+    './src/js/bootstrap.bundle.min.js',
+    './src/js/app.js',
+    './images/layers.png',
+    './images/layers-2x.png',
+    './images/marker-icon.png',
+    './images/marker-icon-2x.png',
+    './images/marker-shadow.png',
 ];
 
 self.addEventListener('install', event => {
